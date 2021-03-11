e813c3ab1e5acf21c36ed264e1327d70afbfa770
studentrep <tug05235@temple.edu>
2021-03-11 15:40:10 +0100

    updated after Javascript (lesson 2)

--- Learning Javascript/Script1.js
+++ Learning Javascript/Script1.js
@@ -1,19 +1,38 @@
 // JavaScript source code
 
-//making elements
-var title = document.createElement("h2")
-title.innerText = "Title"
-var text = document.createElement("p")
-text.innerText = "paragraph"
+//Making Elements
+function addElement() {
+    var title = document.createElement("h2")
+    title.innerText = "Title"
+    var text = document.createElement("p")
+    text.innerText = "paragraph"
 
-//appending elements to HTML document
-document.body.appendChild(title)
-document.body.appendChild(text) 
+    //Appending elements
+    document.body.appendChild(title)
+    document.body.appendChild(text) 
+}
 
-//modify element
-var y = document.getElementById("change")
-y.innerHTML = "Element Changed"
+//Modify Element
+function modifyContent(){
+    var y = document.getElementById("change")
+    y.innerHTML = "Element Changed"
+}
 
-//change attribute
-var img = document.getElementById("testimg")
-img.src = "https://upload.wikimedia.org/wikipedia/commons/7/73/Javascript-736400_960_720.png"
+
+//Change Attribute
+function changeAttribute() {
+    var img = document.getElementById("testimg")
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/7/73/Javascript-736400_960_720.png"
+}
+
+//Change CSS
+function changeColor(color, id) {
+    var element = document.getElementById(id)
+    element.style.color = color
+}
+
+//Change to dark class
+function darkMode() {
+     document.body.classList.add("dark")
+}
+
